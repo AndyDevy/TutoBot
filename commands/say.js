df49deeba7314dcdbc255d218192d2bc8bb9e38a
AndyDevy <andres@andrescaraballo.com>
2018-07-21 15:53:15 -0400

    modified say

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ let config = require('./../config.json');
 
 exports.run = (message, client, args, Discord) => {
 	// Estableciendo argumentos
-	var content = message.replace(config.prefix, '');
+	var content = "data-123".replace('data-','');
 	console.log(content);
 
 	// Tomar el mensaje completo
